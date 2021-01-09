f14a1b665da09d41fd05f11690e03e5f09fa9125
Takashi Ando <takashi7ando@gmail.com>
2021-01-09 17:18:15 +0000

    Run with named container of ddns-updater

--- ddns-updater/run.sh
+++ ddns-updater/run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 source env.sh
-docker run --rm -d -e USER="${USER}" -e PASSWORD="${PASSWORD}" "${IMAGE}"
-
+docker run \
+  --name "${NAME}" \
+  --detach \
+  -e USER="${USER}" \
+  -e PASSWORD="${PASSWORD}" \
+  "${IMAGE}"
